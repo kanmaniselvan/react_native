174d388f569d347fc3668814f03d169cd0a3de52
Ashok Muralidharan <ashok.muralidharan@in.reportbee.com>
2016-11-11 19:09:00 +0530

    Fixes in listing and search functionality

--- ShoppingApp/index.android.js
+++ ShoppingApp/index.android.js
@@ -7,70 +7,80 @@
       ListView,
       Text,
       TouchableOpacity,
-      View
+      View,
+      Image,
+      TextInput
     } from 'react-native';
 
+    var ds_new = new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2 })
 
-    let products = {
-      "SD2014-CF-P": {
-        "category": "electronics",
-        "name": "Nexus 6",
-        "price": 3500,
-        "brand": "LG",
-        "sku": "SD2014-CF-P"
-      },
-      "SD2015-CF-P": {
-        "category": "electronics",
-        "name": "Apple Watch",
-        "price": 6000,
-        "brand": "Apple",
-        "sku": "SD2015-CF-P"
-      },
-      "SD2016-CF-P": {
-        "category": "electronics",
-        "name": "Havels Switch",
-        "price": 120,
-        "brand": "Havels",
-        "sku": "SD2016-CF-P"
-      },
-      "SD2017-CF-P": {
-        "category": "electronics",
-        "name": "Laser Mouse",
-        "price": 450,
-        "brand": "Logitech",
-        "sku": "SD2017-CF-P"
-      },
-      "SD2018-CF-P": {
-        "category": "electronics",
-        "name": "Mini Keyboard",
-        "price": 850,
-        "brand": "Logitech",
-        "sku": "SD2018-CF-P"
-      },
-      "SD2019-CF-P": {
-        "category": "clothing",
-        "name": "Tracks",
-        "price": 120,
-        "brand": "Nike",
-        "sku": "SD2019-CF-P"
-      },
-      "SD2020-CF-P": {
-        "category": "clothing",
-        "name": "Swim Suit",
-        "price": 120,
-        "brand": "Puma",
-        "sku": "SD2020-CF-P"
-      }
-    }
+    let products = [
+                {
+                    "category": "electronics",
+                    "name":"Nexus 6",
+                    "price": 3500,
+                    "brand": "LG",
+                    "sku": "SD2014-CF-P",
+                    "image_url": "https://rukminim1.flixcart.com/image/832/832/mobile/m/v/z/motorola-nexus-6-original-imaefwsvf42tsb4x.jpeg?q=70"
+                },
+                {
+                    "category": "electronics",
+                    "name":"Apple Watch",
+                    "price": 6000,
+                    "brand":"Apple",
+                    "sku":"SD2015-CF-P",
+                    "image_url": "https://rukminim1.flixcart.com/image/832/832/smartwatch/z/t/h/mj3t2hn-a-apple-original-imaect6cuvgzbhxt.jpeg?q=70"
+                },
+                {
+                    "category": "electronics",
+                    "name":"Havels Switch",
+                    "price": 120,
+                    "brand":"Havels",
+                    "sku":"SD2016-CF-P",
+                    "image_url": "https://rukminim1.flixcart.com/image/832/832/electrical-switch/z/p/9/bell-switch-905-le-figaro-original-imaeegthnphcgggg.jpeg?q=70"
+                },
+                {
+                    "category": "electronics",
+                    "name":"Laser Mouse",
+                    "price": 450,
+                    "brand":"Logitech",
+                    "sku":"SD2017-CF-P",
+                    "image_url": "https://rukminim1.flixcart.com/image/832/832/mouse/b/j/g/logitech-b175-original-imae8gb4z8hshfc3.jpeg?q=70"
+                },
+                {
+                    "category": "electronics",
+                    "name":"Mini Keyboard",
+                    "price": 850,
+                    "brand":"Logitech",
+                    "sku":"SD2018-CF-P",
+                    "image_url": "https://rukminim1.flixcart.com/image/832/832/keyboard/laptop-keyboard/g/9/z/logitech-mk240-nano-original-imaez2djyy5pxxyj.jpeg?q=70"
+                },
+                {
+                    "category": "clothing",
+                    "name":"Tracks",
+                    "price": 120,
+                    "brand":"Nike",
+                    "sku":"SD2019-CF-P",
+                    "image_url": "https://rukminim1.flixcart.com/image/832/832/track-pant/f/z/y/83188618-scooter-puma-m-original-imaehb744ckeyavz.jpeg?q=70"
+                },
+                {
+                    "category": "clothing",
+                    "name":"Suit",
+                    "price": 120,
+                    "brand":"Puma",
+                    "sku":"SD2020-CF-P",
+                    "image_url": "https://rukminim1.flixcart.com/image/832/832/blazer/x/z/v/nl-ince-blbq28l-blackberrys-46-original-imaeju2hwhrthgzq.jpeg?q=70"
+                }
+            ]
 
-    var getProductNames = function(products) {
+    /*var getProductNames = function(products) {
       var product_names = [];
-      for (let key in products) {
+      for(var i=0; i<products.length; i++)
         let product = products[key];
         product_names.push(product.name);
       }
       return product_names;
-    }
+    }*/
 
     export default class ShoppingApp extends Component {
 
@@ -80,7 +90,7 @@
           rowHasChanged: (r1, r2) => r1 !== r2
         });
         this.state = {
-          dataSource: ds.cloneWithRows(getProductNames(products)),
+          dataSource: ds.cloneWithRows(products),
         }
       }
 
@@ -124,42 +134,57 @@
 
 
       renderItems(rowData) {
+
+        var openPopup = () => {
+        return(
+          <View>
+          <Text>Hello</Text>
+          </View>
+        )
+        }
+
         return (
-          <TouchableOpacity onPress={this.openPopup}>
+          <TouchableOpacity onPress={openPopup}>
           <View style={styles.container}>
-         
-          <Text style={styles.title}>{rowData}</Text>
+          <Image
+          source={{uri: rowData.image_url}}
+          style={styles.thumbnail}
+          />
+          <View style={styles.rightContainer}>
+          <Text style={styles.title}>{rowData.name}</Text>
+          </View>
           </View>
           </TouchableOpacity>
         );
       }
 
-
-      openPopup() {
-        Alert.alert(
-          'Alert Title',
-          'Alert', [{
-            text: 'Ask me later',
-            onPress: () => console.log('Ask me later pressed')
-          }, {
-            text: 'Cancel',
-            onPress: () => console.log('Cancel Pressed'),
-            style: 'cancel'
-          }, {
-            text: 'OK',
-            onPress: () => console.log('OK Pressed')
-          }, ]
-        );
+      setSearchText(event) {
+        var rows = [];
+         var searchText = event.nativeEvent.text;
+         var search_prod = []
+         for(var i=0; i<products.length; i++) {
+            if(products[i].name.toLowerCase().search(searchText.toLowerCase()) !== -1){
+              rows.push(products[i]);
+            }
+         }
+          this.setState({dataSource:ds_new.cloneWithRows( rows ) });
       }
-
 
       render() {
         return (
+          <View>
+          <TextInput
+         style={styles.searchBar}
+         value={this.state.searchText}
+         onChange={this.setSearchText.bind(this)}
+         placeholder='Search' />   
+
           <ListView tabLabel="ITEMS"
           dataSource={this.state.dataSource}
           renderRow={this.renderItems}
           style={styles.listView}
           />
+          </View>
         );
       }
 
@@ -173,16 +198,17 @@
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+        padding: 10
       },
       rightContainer: {
         flex: 1,
       },
       thumbnail: {
-        width: 53,
-        height: 81,
+        width: 80,
+        height: 100,
       },
       title: {
-        fontSize: 40,
+        fontSize: 20,
         marginBottom: 15,
         padding: 20,
         textAlign: 'center',
